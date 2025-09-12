a25869f0dae765097ed96e470320c66bc7e5bb11
Italo <italo520@gmail.com>
2025-09-12 12:39:00 -0300

    docker funcionando e criação do projeto tabem

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -5,9 +5,11 @@ from apps.contratos.models import Contrato
 class ProjetoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projeto
-        fields = ['id', 'nome', 'status', 'data_inicio', 'data_fim', 'custo_previsto', 'custo_realizado']
+        fields = ['cod_projeto', 'nome', 'situacao', 'data_inicio', 'data_encerramento', 'custo_previsto', 'custo_realizado']
 
 class ContratoSerializer(serializers.ModelSerializer):
+    projeto = serializers.StringRelatedField(source='cod_ordem.cod_requisicao.cod_projeto')
+
     class Meta:
         model = Contrato
-        fields = ['id', 'numero_contrato', 'projeto', 'prestador', 'valor_total', 'status']
+        fields = ['num_contrato', 'projeto', 'contratado', 'valor', 'situacao']
--- apps/projetos/views/projeto_views.py
+++ apps/projetos/views/projeto_views.py
@@ -1,6 +1,8 @@
 # apps/projetos/views/projeto_views.py
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from ..models.projeto import Projeto
 from ..forms.projeto_forms import ProjetoForm
@@ -10,7 +12,7 @@ class ProjetoListView(LoginRequiredMixin, ListView):
     View para listar todos os projetos.
     """
     model = Projeto
-    template_name = 'projetos/projeto_list.html' # <-- Precisaremos criar este template
+    template_name = 'projetos/projeto_list.html'
     context_object_name = 'projetos'
     paginate_by = 10
 
@@ -19,44 +21,58 @@ class ProjetoDetailView(LoginRequiredMixin, DetailView):
     View para ver os detalhes de um projeto específico.
     """
     model = Projeto
-    template_name = 'projetos/projeto_detail.html' # <-- Precisaremos criar este template
+    template_name = 'projetos/projeto_detail.html'
     context_object_name = 'projeto'
 
-class ProjetoCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+class ProjetoCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     """
     View para criar um novo projeto.
     """
     model = Projeto
     form_class = ProjetoForm
-    template_name = 'projetos/projeto_form.html' # <-- Precisaremos criar este template
-    success_url = reverse_lazy('projetos:projeto_list')
-    permission_required = 'projetos.add_projeto'
+    template_name = 'projetos/projeto_form.html'
+    success_message = "Projeto criado com sucesso!"
+
+    def get_success_url(self):
+        return reverse_lazy('projetos:projeto_detail', kwargs={'pk': self.object.pk})
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
-class ProjetoUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    def form_invalid(self, form):
+        messages.error(self.request, "Erro ao criar o projeto. Verifique os campos abaixo.")
+        return super().form_invalid(form)
+
+class ProjetoUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     View para editar um projeto existente.
     """
     model = Projeto
     form_class = ProjetoForm
-    template_name = 'projetos/projeto_form.html' # Reutiliza o mesmo template do Create
-    success_url = reverse_lazy('projetos:projeto_list')
+    template_name = 'projetos/projeto_form.html'
+    success_message = "Projeto atualizado com sucesso!"
     permission_required = 'projetos.change_projeto'
+
+    def get_success_url(self):
+        return reverse_lazy('projetos:projeto_detail', kwargs={'pk': self.object.pk})
 
     def form_valid(self, form):
         form.instance.updated_by = self.request.user
         return super().form_valid(form)
 
-class ProjetoDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    def form_invalid(self, form):
+        messages.error(self.request, "Erro ao atualizar o projeto. Verifique os campos abaixo.")
+        return super().form_invalid(form)
+
+class ProjetoDeleteView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, DeleteView):
     """
     View para deletar um projeto.
     """
     model = Projeto
-    template_name = 'projetos/projeto_confirm_delete.html' # <-- Precisaremos criar este template
+    template_name = 'projetos/projeto_confirm_delete.html'
     success_url = reverse_lazy('projetos:projeto_list')
+    success_message = "Projeto deletado com sucesso!"
     permission_required = 'projetos.delete_projeto'
 
 # As outras views (ChangeStatus, etc.) podem ser adicionadas aqui depois.
